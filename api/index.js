9e9de2583fec8f39efd71f392bd7a2cee6d6f01c
duongnt <duong2932004@gmail.com>
2025-07-17 15:54:52 +0700

    commit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
+const app = require("../index.js");
 
 module.exports = app;
